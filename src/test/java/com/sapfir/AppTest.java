40f5c01beeb32526b7eeb4da8401a9d65be0a8bb
Oleksii Kletsov <okletsov@cainc.com>
2018-08-06 15:54:43 -0400

    First commit

---
+++ src/test/java/com/sapfir/AppTest.java
@@ -0,0 +1,19 @@
+package com.sapfir;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
