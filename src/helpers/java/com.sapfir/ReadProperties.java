1dd53125d98fb2181c5e6c4f064eac5da73978f2
Oleksii Kletsov <akletsov88@gmail.com>
2018-08-12 20:47:25 -0400

    Added ReadProperties helper class

--- src/helpers/java/com.sapfir/ReadProperties.java
+++ src/helpers/java/com.sapfir/ReadProperties.java
@@ -1,10 +1,32 @@
 package com.sapfir;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 public class ReadProperties {
 
-    private ReadProperties (Properties prop) {
-        
+    private String databaseURL;
+    private String databaseUsername;
+    private String databasePassword;
+
+    public ReadProperties() {
+        Properties prop = new Properties();
+
+        try{
+            FileInputStream fileStream = new FileInputStream("config_example.properties");
+            prop.load(fileStream);
+        } catch (IOException e){
+            System.out.println("Error message: " + e.getMessage());
+        }
+
+        this.databaseURL = prop.getProperty("database_url");
+        this.databaseUsername = prop.getProperty("database_username");
+        this.databasePassword = prop.getProperty("database_password");
     }
+
+    public String getDatabaseURL() {return databaseURL;}
+    public String getDatabaseUsername() {return databaseUsername;}
+    public String getDatabasePassword() {return databasePassword;}
+
 }
--- src/test/java/com/sapfir/Initialize_Contest_Test.java
+++ src/test/java/com/sapfir/Initialize_Contest_Test.java
@@ -3,29 +3,16 @@ package com.sapfir;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
-
 public class Initialize_Contest_Test {
-
-	private Properties prop;
 
 	@BeforeClass
 	public void setUp() {
-		prop = new Properties();
 
-		// Loading configuration properties
-		try{
-			FileInputStream fileStream = new FileInputStream("config_example.properties");
-			prop.load(fileStream);
-		} catch (IOException e){
-			System.out.println("Error message: " + e.getMessage());
-		}
 	}
 
 	@Test
 	public void test() {
-		System.out.println(prop.get("database_url"));
+		ReadProperties rp = new ReadProperties();
+		System.out.println(rp.getDatabaseURL());
 	}
 }
