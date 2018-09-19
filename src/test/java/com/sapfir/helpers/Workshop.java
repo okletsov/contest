d63a702d6811252d7d2c0edd3ae1e9ae88fd3932
Oleksii Kletsov <akletsov88@gmail.com>
2018-09-18 22:47:06 -0400

    wip

--- src/test/java/com/sapfir/helpers/Workshop.java
+++ src/test/java/com/sapfir/helpers/Workshop.java
@@ -12,5 +12,11 @@ public class Workshop {
         Date date = Date.from(Instant.ofEpochSecond(unixDate));
         LocalDateTime localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
         System.out.println(localDate);
+
+        System.out.println(localDate.getYear());
+        System.out.println(localDate.getMonthValue());
+        System.out.println(localDate.getDayOfMonth());
+        System.out.println(localDate.getHour());
+        System.out.println(localDate.getMinute());
     }
 }
