2979355c7501cd7ac8f6553a1c219ec039ff53e4
Oleksii Kletsov <akletsov88@gmail.com>
2018-09-30 10:48:12 -0400

    wip

--- src/test/java/com/sapfir/pageClasses/PredictionsInspection.java
+++ src/test/java/com/sapfir/pageClasses/PredictionsInspection.java
@@ -5,7 +5,6 @@ import com.sapfir.helpers.SeleniumMethods;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
@@ -123,7 +122,7 @@ public class PredictionsInspection {
 
     public int getUserPick(String predictionID) {
         Log.debug("Getting user pick index...");
-        Integer index = 5;
+        int index = 5;
 
         List<WebElement> pickColumns = driver.findElements(By.cssSelector(
                 "#" + predictionID + "  [class='pred-usertip'] td"));
