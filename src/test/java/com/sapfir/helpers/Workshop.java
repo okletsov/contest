c9638b04659655233ff808251b47a29bf3380825
Oleksii Kletsov <okletsov@cainc.com>
2018-10-01 15:28:28 -0400

    wip

--- src/test/java/com/sapfir/helpers/Workshop.java
+++ src/test/java/com/sapfir/helpers/Workshop.java
@@ -8,12 +8,20 @@ import java.util.Date;
 public class Workshop {
     public static void main(String[] args) {
 
-        Long timestamp = Long.parseLong("1451748600");
-        Date date = Date.from(Instant.ofEpochSecond(timestamp));
-        LocalDateTime localDate = date.toInstant().atZone(ZoneOffset.UTC).toLocalDateTime();
-        System.out.println(localDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        String unixDate = "1538131200";
+        Long longDate = Long.parseLong(unixDate);
+        Instant instantDateTime = Instant.ofEpochSecond(longDate);
+        ZonedDateTime zonedDateTime = instantDateTime.atZone(ZoneOffset.UTC);
+        String finalDateTime = zonedDateTime.format(DateTimeFormatter.ofPattern("yyyy-mm-dd hh:mm:ss"));
 
-        System.out.println(ZonedDateTime.now().toLocalDateTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        String date =
+                Instant.ofEpochSecond(Long.parseLong(unixDate)).atZone(ZoneOffset.UTC).format(DateTimeFormatter.ofPattern("yyyy-mm-dd hh:mm:ss"));
+
+        System.out.println(instantDateTime);
+        System.out.println(zonedDateTime);
+        System.out.println(finalDateTime);
+
+        System.out.println(date);
 
 
     }
