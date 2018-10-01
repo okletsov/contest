a9b5124f39425a3f7311884feafeeb3ff2f4c60e
Oleksii Kletsov <okletsov@cainc.com>
2018-10-01 11:32:24 -0400

    wip

--- src/test/java/com/sapfir/helpers/Workshop.java
+++ src/test/java/com/sapfir/helpers/Workshop.java
@@ -1,15 +1,20 @@
 package com.sapfir.helpers;
 
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class Workshop {
     public static void main(String[] args) {
+
+        Long timestamp = Long.parseLong("1451748600");
+        Date date = Date.from(Instant.ofEpochSecond(timestamp));
+        LocalDateTime localDate = date.toInstant().atZone(ZoneOffset.UTC).toLocalDateTime();
+        System.out.println(localDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+
         System.out.println(ZonedDateTime.now().toLocalDateTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+
+
     }
 }
