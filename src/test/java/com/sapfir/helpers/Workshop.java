a0bca7cd704259ae2793a2f7cd08c23cfb959e63
Oleksii Kletsov <akletsov88@gmail.com>
2018-10-27 11:17:51 -0400

    wip

--- src/test/java/com/sapfir/helpers/Workshop.java
+++ src/test/java/com/sapfir/helpers/Workshop.java
@@ -4,26 +4,62 @@ package com.sapfir.helpers;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.List;
 
 public class Workshop {
     public static void main(String[] args) {
+        String[] compArray = new String[50];
+        compArray[0] = "Wang Q. - Yastremska D.";
+        compArray[1] = "Wang Q. - Muguruza G.";
+        compArray[2] = "Yastremska D. - Zhang S.";
+        compArray[3] = "Svitolina E. - Wang Q.";
+        compArray[4] = "Muguruza G. - Kumkhum L.";
+        compArray[5] = "Gavrilova D. - Zhang S.";
+        compArray[6] = "Yastremska D. - Kucova K.";
+        compArray[7] = "Svitolina E. - Hibino N.";
+        compArray[8] = "McHale C. - Wang Q.";
+        compArray[9] = "Kerkhove L. - Zhang S.";
+        compArray[10] = "Gavrilova D. - Niculescu M.";
+        compArray[11] = "Muguruza G. - Bogdan A.";
+        compArray[12] = "Tomova V. - Kucova K.";
+        compArray[13] = "Zheng S. - Yastremska D.";
+        compArray[14] = "Kumkhum L. - Cornet A.";
+        compArray[15] = "Svitolina E. - Hon P.";
+        compArray[16] = "Muguruza G. - Sorribes Tormo S.";
+        compArray[17] = "Dolehide C. - Bogdan A.";
+        compArray[18] = "Sharipova S. - Zhang S.";
+        compArray[19] = "Schoofs B. - Kerkhove L.";
+        compArray[20] = "Stosur S. - Hibino N.";
+        compArray[21] = "Zhang L. - Wang Q.";
+        compArray[22] = "Gavrilova D. - Diyas Z.";
+        compArray[23] = "McHale C. - Chong E. W.";
+        compArray[24] = "Niculescu M. - Jabeur O.";
+        compArray[25] = "Krunic A. - Zheng S.";
+        compArray[26] = "Kucova K. - Ostapenko J.";
+        compArray[27] = "Glushko J. - Cornet A.";
+        compArray[28] = "Brady J. - Kumkhum L.";
+        compArray[29] = "Tomova V. - Jakupovic D.";
+        compArray[30] = "Yastremska D. - Stollar F.";
+        compArray[31] = "Niculescu M. - Makarova E.";
+        compArray[32] = "Stosur S. - Krunic A.";
+        compArray[33] = "Tsurenko L. - Zheng S.";
+        compArray[34] = "Zhang S. - Cornet A.";
+        compArray[35] = "Hibino N. - Zhao C.";
+        compArray[36] = "Dolehide C. - Kerkhove L.";
+        compArray[37] = "Stollar F. - Siegemund L.";
+        compArray[38] = "Lu J. - Sharipova S.";
+        compArray[39] = "Glushko J. - Schoofs B.";
+        compArray[40] = "Jabeur O. - Tomova V.";
+        compArray[41] = "Dolehide C. - Wu Ho C.";
+        compArray[42] = "Melnikova M. - Sharipova S.";
+        compArray[43] = "Stollar F. - Ng K. Y.";
+        compArray[44] = "Kerkhove L. - Liu C.";
+        compArray[45] = "Plipuech P. - Siegemund L.";
+        compArray[46] = "Zhao C. - Mrdeza T.";
+        compArray[47] = "Ng M. - Schoofs B.";
+        compArray[48] = "Watson H. - Lu J.";
+        compArray[49] = "Hibino N. - Shimizu A.";
 
-        String unixDate = "1538131200";
-        Long longDate = Long.parseLong(unixDate);
-        Instant instantDateTime = Instant.ofEpochSecond(longDate);
-        ZonedDateTime zonedDateTime = instantDateTime.atZone(ZoneOffset.UTC);
-        String finalDateTime = zonedDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-
-        String date =
-                Instant.ofEpochSecond(Long.parseLong(unixDate)).atZone(ZoneOffset.UTC).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-
-        System.out.println(instantDateTime);
-        System.out.println(zonedDateTime);
-        System.out.println(finalDateTime);
-
-        System.out.println(date);
-
-        String now = ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        System.out.println(now);
+        String betText = "Gavrilova Daria";
     }
 }
