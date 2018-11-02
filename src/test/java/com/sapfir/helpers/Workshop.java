fb6c2296c21fb0d663903b35c60670b7a712d8a0
Oleksii Kletsov <akletsov88@gmail.com>
2018-11-01 22:19:05 -0400

    wip

--- src/test/java/com/sapfir/helpers/Workshop.java
+++ src/test/java/com/sapfir/helpers/Workshop.java
@@ -38,12 +38,12 @@ public class Workshop {
         compArray[26] = "Kucova K. - Ostapenko J.";
         compArray[27] = "Glushko J. - Cornet A.";
         compArray[28] = "Brady J. - Kumkhum L.";
-        compArray[29] = "Tomova V. - Dyn. Kyiv";
+        compArray[29] = "Tomova V. - Dynamo Kyiv";
         compArray[30] = "Yastremska D. - Stollar F.";
         compArray[31] = "Niculescu M. - Makarova E.";
         compArray[32] = "Stosur S. - Krunic A.";
         compArray[33] = "Tsurenko L. - Zheng S.";
-        compArray[34] = "Zhang S. - Cornet A.";
+        compArray[34] = "Shakhtar Donetsk - Cornet A.";
         compArray[35] = "Hibino N. - Zhao C.";
         compArray[36] = "Dolehide C. - Kerkhove L.";
         compArray[37] = "Stollar F. - Siegemund L.";
@@ -54,74 +54,37 @@ public class Workshop {
         compArray[42] = "Melnikova M. - Sharipova S.";
         compArray[43] = "D. Zagreb - Ng K. Y.";
         compArray[44] = "Kerkhove L. - Liu C.";
-        compArray[45] = "Shakhtar Donetsk - Siegemund L.";
+        compArray[45] = "Glushko J. - Siegemund L.";
         compArray[46] = "Zhao C. - Mrdeza T.";
         compArray[47] = "Ng M. - Schoofs B.";
         compArray[48] = "Watson H. - FK Crvena zvezda";
         compArray[49] = "Hibino N. - Shimizu A.";
 
-        /*
-            Variables:
-                betTeam - the team/person user predicted to win a tournament (OUTRIGHTS market)
-                compArray - array storing event results for the tournament
-
-            Goal:
-                find index (first occurrence) of betTeam in compArray.
-                The index will be used to determine the datetime event occurred
-
-            How method works:
-                Because team name in OUTRIGHTS and tournament RESULTS pages not always match, method will
-                do the following:
-
-                if betText consist of only one word the method will try to find that word in compaArray
-                if betText consist of > 1 words:
-                    1) it will try to find betText in compArray, if mno match found then
-                    2) it will go word by word in betText and will:
-                        - replace the word in betText with empty string --> try to find match in compAtrray
-                        - replace the word in betText with its first letter --> try to find match in compAtrray
-                        - replace the word in betText with its first two letters --> try to find match in compAtrray
-                        - replace the word in betText with its first three letters --> try to find match in compAtrray
-                        - replace the word in betText with its first four letters --> try to find match in compAtrray
-                        - ...
-
-             Can return wrong index if:
-                - betText consists of > 1 words
-                  AND one of the words shortened
-                  AND there are two teams with same NOT shortened word playing in tournament
-
-                  Example: betText can be Williams Serena or Williams Venus
-                           compArray is shortened to Williams S. and Williams. V
-                           method will return wrong result if user predicted Williams Serena to win, but Venus advanced
-                           to later stages (her games were after last Serena's) comparing to Serena
-
-            Think how to handle Pliskova example above
-             - user bets on Karolina, but Kristina wins (method will return Kristina's result)
-         */
-        String betTeam = "Dynamo Kyiv";
+        String betTeam = "Shakhtar".replace(".", "");
 
         boolean matchFound = false;
-        int i = 0;
-        while (!matchFound && i < compArray.length) {
-            String competitorsLine = compArray[i].replace(".", "");
-            matchFound = competitorsLine.contains(betTeam);
-            String[] words = betTeam.split(" ");
-            if (!matchFound && words.length > 1) {
-                int j = 0;
-                while (!matchFound && j < words.length) {
-                    char[] letters = words[j].toCharArray();
-                    int k = 0;
-                    while (!matchFound && k < letters.length) {
-                        String newWord = words[j].substring(0, k);
-                        matchFound = competitorsLine.contains(betTeam.replace(words[j], newWord));
-                        k++;
-                    }
-                    j++;
-                }
-            }
-            i++;
-        }
+        int indexFound = -1;
 
-        int indexFound = i - 1;
+        String[] words = betTeam.split(" ");
+        int j = 0;
+        while (!matchFound && j < words.length) {
+            char[] letters = words[j].toCharArray();
+            int k = letters.length - 1;
+            while (!matchFound && k >= 0) {
+                String newWord = words[j].substring(0, k);
+                int i = 0;
+                while (!matchFound && i < compArray.length) {
+                    String competitorsLine = compArray[i].replace(".", "");
+                    matchFound = competitorsLine.contains(betTeam.replace(words[j], newWord));
+                    if (matchFound) {
+                        indexFound = i;
+                    }
+                    i++;
+                }
+                k--;
+            }
+            j++;
+        }
         System.out.println(matchFound + " " + indexFound);
     }
 }
