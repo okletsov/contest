46d96f8985cc8a36f925747f38b46da4d7eca212
Oleksii Kletsov <akletsov88@gmail.com>
2020-07-04 10:23:34 -0400

    Removed old PredictionValidation class

--- src/test/java/com/sapfir/helpers/Contest.java
+++ src/test/java/com/sapfir/helpers/Contest.java
@@ -104,64 +104,6 @@ public class Contest {
 		return getEndDate().plusHours(24);
 	}
 
-	public LocalDateTime getSeasStartDate() {
-		String sql = "select start_date from contest where id = '" + contestId + "';";
-		String stringStartDate = dbOp.getSingleValue(conn, "start_date", sql);
-		return dtOp.convertToDateTimeFromString(stringStartDate);
-	}
-
-	public LocalDateTime getSeasEndDate() {
-		String sql = "select end_date from contest where id = '" + contestId + "';";
-		String stringEndDate = dbOp.getSingleValue(conn, "end_date", sql);
-		return dtOp.convertToDateTimeFromString(stringEndDate);
-	}
-
-	public LocalDateTime getSeasEndDate24() {
-		LocalDateTime seasEndDate = getSeasEndDate();
-		return seasEndDate.plusHours(24);
-	}
-
-	public LocalDateTime getSeasLastDayStart() {
-		LocalDateTime seasEndDate = getSeasEndDate();
-		return seasEndDate.minusHours(24);
-	}
-
-	public LocalDateTime getMonStartDate(int monthIndex){
-		String sql = "select c2.start_date " +
-				"from contest c1 " +
-				"join contest c2 on c1.year = c2.year " +
-				"and c1.season = c2.season " +
-				"where c1.id = '" + contestId + "' " +
-				"and c2.month = " + monthIndex + "; ";
-
-		String stringStartDate = dbOp.getSingleValue(conn, "start_date", sql);
-		return dtOp.convertToDateTimeFromString(stringStartDate);
-	}
-
-	public LocalDateTime getMonEndDate(int monthIndex){
-		String sql = "select c2.end_date " +
-				"from contest c1 " +
-				"join contest c2 on c1.year = c2.year " +
-				"and c1.season = c2.season " +
-				"where c1.id = '" + contestId + "' " +
-				"and c2.month = " + monthIndex + "; ";
-
-		String stringStartDate = dbOp.getSingleValue(conn, "end_date", sql);
-		return dtOp.convertToDateTimeFromString(stringStartDate);
-	}
-
-	public LocalDateTime getMonEndDate24(int monthIndex) {
-		// This method adds 24h to month end date
-		LocalDateTime monEndDate = getMonEndDate(monthIndex);
-		return monEndDate.plusHours(24);
-	}
-
-	public LocalDateTime getMonLastDayStart(int monthIndex) {
-		//This method subtracts 23hr 59m 59s from month end date
-		LocalDateTime monEndDate = getMonEndDate(monthIndex);
-		return monEndDate.minusSeconds(86399);
-	}
-
 	public ArrayList<String> getPredictionsToValidate() {
 		String sql = "select \n" +
 				"\tt2.id\n" +
